c77b65ea98d73cfd8c1e397cd2490758f2e5a7fb
Shruti Jog <shruti.jog@whitecode.co.in>
2025-08-24 23:23:25 +0530

    welcome msg replaced

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
    <Router>
-    <h3>Welcome</h3>
+    <h3>Multi-tenant example</h3>
       <Routes>
          
         <Route  path="/:tenant" element={<FacultyPage />} />
